c2c9600c43792bb01f1d241477c4b39bf57d3d79
Nathan Smith <mcaeolusch@gmail.com>
2019-10-09 23:50:31 -0400

    Update HNAMES_RUCKUS.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverruckus/HNAMES_RUCKUS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverruckus/HNAMES_RUCKUS.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.roverruckus;
 public class HNAMES_RUCKUS {
 
     public static final String
-            VUFORIA_KEY = "AX+gX5z/////AAAAGemESFRCSEr9mlgYKyJlg7QkunLknhP5uXkpeqYVGio+FNmaVefvE0yC0ueWXyecPzSipBsPHsGK1aduCXc87+J6jRilxDjmP8aSmRGOopchZ7KhKbpKRLtcP+i408NrfCRzT1CVhaQ9YI3ObQApDcyNxoiz5G8TVCh7SIr1qL0NXHW1QEuCgGP2BhSJDhUomuotPTd11WcfoMyFqBg2FUb3iC1Msv4iIzUirCRkQU3boFywhIVSZ3cvM+1IWDh/AjMgNZ1Fy/jueNKirAQqQVDqC5cYn9aAvEpewgJAwMjYlaYaXSjb+hLkyh1+ZY2MLsZAbed3pUGJ8eI65QgI3NDGtJs3XL7r8rXoIVvnoezg\n",
+            VUFORIA_KEY = "",
             DRIVE_MOTORFL = "driveFL",
             DRIVE_MOTORFR = "driveFR",
             DRIVE_MOTORBL = "driveBL",
